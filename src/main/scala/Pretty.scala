d731dde207655c9c3f19c7f3a10881405d6fe5f7
Aaron Weiss <aaronweiss74@gmail.com>
2016-10-23 03:07:20 -0400

    Implemented pretty printing for built-ins.

--- src/main/scala/Pretty.scala
+++ src/main/scala/Pretty.scala
@@ -26,9 +26,17 @@ object Pretty {
     case EOp2(op, lhs, rhs) => s"(${prettyExpr(lhs)} ${prettyOp2(op)} ${prettyExpr(rhs)})"
     case EFun(id, body) => s"($id => ${prettyExpr(body)})"
     case EApp(fun, arg) => s"$fun($arg)"
-    case EBuiltIn(builtIn, args) => ???
+    case EBuiltIn(builtIn, args) => s"${prettyBuiltIn(builtIn)}(" + args.tail.foldLeft(prettyExpr(args.head))((acc, expr) => s"$acc, ${prettyExpr(expr)}") + ")"
     case EIf(pred, tru, fls) => s"if $pred then $tru else $fls"
     case EBlock(exprs) => "{ " + exprs.tail.foldLeft(prettyExpr(exprs.head))((acc, expr) => s"$acc; ${prettyExpr(expr)}") + " }"
+  }
+
+  def prettyBuiltIn(builtIn: BuiltIn): String = builtIn match {
+    case BShow => "show"
+    case BPrint => "print"
+    case BPrintln => "println"
+    case BCatch => "catch"
+    case BInject => "inject"
   }
 
   def prettyOp1(op1: Op1): String = op1 match {

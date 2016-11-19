b82016754cd179211911b513fd282261343ee82a
Aaron Weiss <aaronweiss74@gmail.com>
2016-11-19 02:36:57 -0500

    Modified pretty printing of types to eliminate numbers from fresh tyvars.

--- src/main/scala/Pretty.scala
+++ src/main/scala/Pretty.scala
@@ -8,21 +8,45 @@ object Pretty {
     case KRow => "e"
   }
 
-  def prettyType(typ: Type): String = typ match {
+  def getTypeVarSimplification(typ: Type): Map[TyVar, String] = {
+    def map2[A, B, C](a: Stream[A], b: Stream[B])(f: (A, B) => C): Stream[C] =
+      a.flatMap { x => b.map { y => f(x, y) } }
+
+    def sequence[A](a: Stream[Stream[A]]): Stream[Stream[A]] =
+      a.foldRight[Stream[Stream[A]]](Stream(Stream()))((x, y) => map2(x, y)(_ #:: _))
+
+    def replicate[A](n: Int, s: Stream[A]): Stream[Stream[A]] =
+      sequence(Stream.fill(n)(s))
+
+    val letterRng = 'a' to 'z'
+    var letters = Stream.from(1).flatMap(n => replicate(n, letterRng.toStream))
+
+    typ.freeTypeVars.map {
+      tv => {
+        val res = tv -> letters.head.mkString
+        letters = letters.tail
+        res
+      }
+    }.toMap
+  }
+
+  def prettyType(typ: Type): String = prettyType(typ, getTypeVarSimplification(typ))
+
+  def prettyType(typ: Type, simp: Map[TyVar, String]): String = typ match {
     case TUnit => "()"
     case TNum => "num"
     case TString => "string"
     case TBool => "bool"
-    case TFun(x, e, y) => s"(${prettyType(x)} -> ${prettyType(e)} ${prettyType(y)})"
-    case TVar(tyvar) => tyvar.name
+    case TFun(x, e, y) => s"(${prettyType(x, simp)} -> ${prettyType(e, simp)} ${prettyType(y, simp)})"
+    case TVar(tyvar) => simp(tyvar)
     case TRowEmpty => "<>"
-    case TRowExtend(_, _, _) => prettyRow(typ)
+    case TRowExtend(_, _, _) => prettyRow(typ, simp)
   }
 
-  def prettyRow(typ: Type): String = InferenceEngine.decomposeRow(typ) match {
+  def prettyRow(typ: Type, simp: Map[TyVar, String]): String = InferenceEngine.decomposeRow(typ) match {
     case (lpairs, Some(tyvar)) => {
       val row = lpairs.map(_._1).mkString(", ")
-      s"<$row | ${tyvar.name}>"
+      s"<$row | ${simp(tyvar)}>"
     }
     case (lpairs, None) => {
       val row = lpairs.map(_._1).mkString(", ")
